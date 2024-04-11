c6e41ab912c64f0ca36eea36544ecc3325d9b610
Martin <107637104+EduarM70@users.noreply.github.com>
2024-04-10 19:45:12 -0600

    Creación Proyecto de Laravel

---
+++ Dailyio-appweb/resources/views/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba</title>
+    @vite('resources/css/app.css')
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">
+</head>
+
+<body class="p-4">
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+</body>
+
+</html>
