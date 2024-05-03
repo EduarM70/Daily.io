02f86b1df78aebd2c8e8bea6d33e9615724da5bf
Rogers Deleon <rogrsdeleon506@gmail.com>
2024-05-02 23:12:58 -0600

    View tareas
    Avance

--- Dailyio-appweb/resources/views/tareas.blade.php
+++ Dailyio-appweb/resources/views/tareas.blade.php
@@ -3,38 +3,71 @@
 
 @section('content')
 <div class="dark:text-white p-4">
-    <div class="">
-        <h2 class="text-4xl font-black uppercase">Tus Tareas</h2> adawdawd
+    <nav class="flex" aria-label="Breadcrumb">
+        <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+            <li class="inline-flex items-center">
+                <a href="#" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                    <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                    </svg>
+                    Inicio
+                </a>
+            </li>
+            <li>
+                <div class="flex items-center">
+                    <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                    </svg>
+                    <a href="#" class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">Tareas</a>
+                </div>
+            </li>
+        </ol>
+    </nav>
+
+    <div class="mt-10">
+        <h2 class="text-4xl font-black uppercase">Tus Tareas</h2>
     </div>
-    <button class="mt-5 middle none center mr-3 rounded-lg bg-gradient-to-tr from-pink-600 to-pink-400 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" data-ripple-light="true">
-        Button
-    </button>
+
+    <div class="inline-flex items-center   space-x-20 mt-5 p-4 w-2/4 text-lg ">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Agregar Tareas +
+        </button>
+        <div class="inline-flex space-x-1.5 font-bold	">
+            <h1><a href="">Personal</a></h1>
+            <p class="">/</p>
+            <h1><a href="">Proyectos</a></h1>
+        </div>
+    </div>
+
 
 
     <div class="flex-1 p-4">
 
         <!-- Contenedor de las 4 secciones (disminuido para dispositivos pequeños) -->
         <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-2 p-2">
-            <!-- Sección 1 - Gráfica de Usuarios (disminuida para dispositivos -->
-
-
             <!-- Sección 1 - Gráfica de Usuarios -->
             <div class="bg-white p-4 rounded-md">
-
-                <div class="flex justify-center items-center bg-gray-100">
-                    <li class="flex items-center">
-                        <a href="#" class="text-gray-600 hover:text-blue-500 transition-colors duration-300">Personal</a>
-                        <span class="mx-2">/</span>
-                    </li>
-                    <li class="flex items-center">
-                        <a href="#" class="text-gray-600 hover:text-blue-500 transition-colors duration-300">Proyectos</a>
-                    </li>
-                </div>
-                <div class="w-full mt-4 py-4 flex items-center">
-                    <span class="block h-3 w-3 bg-violet-500 rounded-full"></span>
-                    <span class="border-slate-400 border-t w-5/6 block"></span>
-                    <h4 class="p-1 px-4 bg-violet-500 text-xs rounded-md text-white">Hoy</h4>
-                </div>
+                <h2 class="text-gray-500 text-lg font-semibold pb-1">Hoy</h2>
+                <div class="my-1"></div> <!-- Espacio de separación -->
+                <div class="bg-gradient-to-r from-cyan-300 to-cyan-500 h-px mb-6"></div> <!-- Línea con gradiente -->
+                <li class="p-2 rounded-lg list-none">
+                    <div class="flex align-middle flex-row justify-between">
+                        <div class="p-2">
+                            <input type="checkbox" class="h-6 w-6 " value="true" />
+                        </div>
+                        <div class="p-2">
+                            <p class="text-lg text-black">Tarea 1</p>
+                        </div>
+                        <button class="flex text-red-500 border-2 border-red-500 p-2 rounded-lg">
+                            <svg class="h-6 w-6 text-red-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                <circle cx="12" cy="12" r="10" />
+                                <line x1="15" y1="9" x2="9" y2="15" />
+                                <line x1="9" y1="9" x2="15" y2="15" />
+                            </svg>
+                            <span>Eliminar</span>
+                        </button>
+                    </div>
+                </li>
 
                 <li class="p-2 rounded-lg list-none">
                     <div class="flex align-middle flex-row justify-between">
@@ -53,15 +86,15 @@
                             <span>Eliminar</span>
                         </button>
                     </div>
-                    <hr class="mt-2" />
                 </li>
+
                 <li class="p-2 rounded-lg list-none">
                     <div class="flex align-middle flex-row justify-between">
                         <div class="p-2">
                             <input type="checkbox" class="h-6 w-6 " value="true" />
                         </div>
                         <div class="p-2">
-                            <p class="text-lg text-black">Tarea 2</p>
+                            <p class="text-lg text-black">Tarea 1</p>
                         </div>
                         <button class="flex text-red-500 border-2 border-red-500 p-2 rounded-lg">
                             <svg class="h-6 w-6 text-red-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
@@ -72,15 +105,15 @@
                             <span>Eliminar</span>
                         </button>
                     </div>
-                    <hr class="mt-2" />
                 </li>
+
                 <li class="p-2 rounded-lg list-none">
                     <div class="flex align-middle flex-row justify-between">
                         <div class="p-2">
                             <input type="checkbox" class="h-6 w-6 " value="true" />
                         </div>
                         <div class="p-2">
-                            <p class="text-lg text-black">Tarea 3</p>
+                            <p class="text-lg text-black">Tarea 1</p>
                         </div>
                         <button class="flex text-red-500 border-2 border-red-500 p-2 rounded-lg">
                             <svg class="h-6 w-6 text-red-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
@@ -91,7 +124,6 @@
                             <span>Eliminar</span>
                         </button>
                     </div>
-                    <hr class="mt-2" />
                 </li>
             </div>
 
@@ -100,15 +132,82 @@
                 <h2 class="text-gray-500 text-lg font-semibold pb-1">Mañana</h2>
                 <div class="my-1"></div> <!-- Espacio de separación -->
                 <div class="bg-gradient-to-r from-cyan-300 to-cyan-500 h-px mb-6"></div> <!-- Línea con gradiente -->
-                <h1>Tareas 1</h1>
-                <h1>Tareas 2</h1>
-                <h1>Tareas 3</h1>
-                <h1>Tareas 4</h1>
-                <h1>Tareas 5</h1>
-                <h1>Tareas 6</h1>
+                <li class="p-2 rounded-lg list-none">
+                    <div class="flex align-middle flex-row justify-between">
+                        <div class="p-2">
+                            <input type="checkbox" class="h-6 w-6 " value="true" />
+                        </div>
+                        <div class="p-2">
+                            <p class="text-lg text-black">Tarea 1</p>
+                        </div>
+                        <button class="flex text-red-500 border-2 border-red-500 p-2 rounded-lg">
+                            <svg class="h-6 w-6 text-red-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                <circle cx="12" cy="12" r="10" />
+                                <line x1="15" y1="9" x2="9" y2="15" />
+                                <line x1="9" y1="9" x2="15" y2="15" />
+                            </svg>
+                            <span>Eliminar</span>
+                        </button>
+                    </div>
+                </li>
+
+                <li class="p-2 rounded-lg list-none">
+                    <div class="flex align-middle flex-row justify-between">
+                        <div class="p-2">
+                            <input type="checkbox" class="h-6 w-6 " value="true" />
+                        </div>
+                        <div class="p-2">
+                            <p class="text-lg text-black">Tarea 1</p>
+                        </div>
+                        <button class="flex text-red-500 border-2 border-red-500 p-2 rounded-lg">
+                            <svg class="h-6 w-6 text-red-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                <circle cx="12" cy="12" r="10" />
+                                <line x1="15" y1="9" x2="9" y2="15" />
+                                <line x1="9" y1="9" x2="15" y2="15" />
+                            </svg>
+                            <span>Eliminar</span>
+                        </button>
+                    </div>
+                </li>
+
+                <li class="p-2 rounded-lg list-none">
+                    <div class="flex align-middle flex-row justify-between">
+                        <div class="p-2">
+                            <input type="checkbox" class="h-6 w-6 " value="true" />
+                        </div>
+                        <div class="p-2">
+                            <p class="text-lg text-black">Tarea 1</p>
+                        </div>
+                        <button class="flex text-red-500 border-2 border-red-500 p-2 rounded-lg">
+                            <svg class="h-6 w-6 text-red-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                <circle cx="12" cy="12" r="10" />
+                                <line x1="15" y1="9" x2="9" y2="15" />
+                                <line x1="9" y1="9" x2="15" y2="15" />
+                            </svg>
+                            <span>Eliminar</span>
+                        </button>
+                    </div>
+                </li>
+
+                <li class="p-2 rounded-lg list-none">
+                    <div class="flex align-middle flex-row justify-between">
+                        <div class="p-2">
+                            <input type="checkbox" class="h-6 w-6 " value="true" />
+                        </div>
+                        <div class="p-2">
+                            <p class="text-lg text-black">Tarea 1</p>
+                        </div>
+                        <button class="flex text-red-500 border-2 border-red-500 p-2 rounded-lg">
+                            <svg class="h-6 w-6 text-red-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                <circle cx="12" cy="12" r="10" />
+                                <line x1="15" y1="9" x2="9" y2="15" />
+                                <line x1="9" y1="9" x2="15" y2="15" />
+                            </svg>
+                            <span>Eliminar</span>
+                        </button>
+                    </div>
+                </li>
             </div>
-
-
         </div>
     </div>
 </div>
