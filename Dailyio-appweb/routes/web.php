371fc81c2baff7d37251563460e5804d4e73b8d8
Jonh <jonh_kevin@live.com.ar>
2024-05-06 17:05:53 -0600

    Agregando Login

--- Dailyio-appweb/routes/web.php
+++ Dailyio-appweb/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +10,11 @@ Route::get('/', function () {
 Route::get('/prueba', function(){
     return view('app');
 });
+
+Route::view('/login', "login")->name('login');
+Route::view('/registro', "register")->name('registro');
+Route::view('/privada', "secret")->middleware('auth')->name('privada');
+
+Route::post('/validar-registro',[LoginController::class,'register'])->name('validar-registro');
+Route::post('/inicia-sesion', [LoginController::class,'login'])->name('inicia-sesion');
+Route::get('/logout',[LoginController::class,'logout'])->name('logout');
